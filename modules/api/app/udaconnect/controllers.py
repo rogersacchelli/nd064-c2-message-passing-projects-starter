358456b052c52a448cecdfe89b2b61ddd46b073e
Roger <roger.sacchelli@gmail.com>
2023-09-10 00:16:12 -0300

    Fix Api

--- modules/api/app/udaconnect/controllers.py
+++ modules/api/app/udaconnect/controllers.py
@@ -20,10 +20,7 @@ api = Namespace("UdaConnect", description="Connections via geolocation.")  # noq
 # TODO: This needs better exception handling
 
 
-@api.route("/locations")
-def get(self):
-    location: Location = LocationService.retrieve_all()
-    return location     
+@api.route("/locations")     
 @api.route("/locations/<location_id>")
 @api.param("location_id", "Unique ID for a given Location", _in="query")
 class LocationResource(Resource):
